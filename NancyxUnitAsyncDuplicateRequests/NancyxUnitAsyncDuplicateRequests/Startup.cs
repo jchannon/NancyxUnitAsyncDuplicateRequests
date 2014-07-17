b6a97a9190b43aa65804e26390c38a320b52fba8
Jonathan Channon <jonathan.channon@gmail.com>
2014-07-17 21:23:26 +0100

    initial

---
+++ NancyxUnitAsyncDuplicateRequests/NancyxUnitAsyncDuplicateRequests/Startup.cs
@@ -0,0 +1,75 @@
+﻿namespace NancyxUnitAsyncDuplicateRequests
+{
+    using System.Diagnostics;
+    using Nancy.Owin;
+    using Nancy;
+    using Owin;
+    using System;
+    using MidFunc = System.Func<
+        System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>,
+    System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>
+    >;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app
+                .Use(MyMiddleware.DoIt())
+                .UseNancy();
+        }
+    }
+
+    public class MyBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            Debug.WriteLine("APP STARTUP");
+
+            pipelines.BeforeRequest += (ctx) =>
+            {
+                var env = ctx.GetOwinEnvironment();
+                Debug.WriteLine("BEFORE REQUEST : " + env["owin.RequestId"]);
+                return null;
+            };
+        }
+
+        protected override void RequestStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(container, pipelines, context);
+            Debug.WriteLine("REQUEST STARTUP");
+        }
+    }
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Boo";
+        }
+    }
+
+    public static class MyMiddleware
+    {
+        public static MidFunc DoIt()
+        {
+            return next => async env =>
+            {
+                string requestId;
+                if (env.ContainsKey("owin.RequestId"))
+                {
+                    requestId = env["owin.RequestId"].ToString();
+                }
+                else
+                {
+                    requestId = Guid.NewGuid().ToString();
+                    env["owin.RequestId"] = requestId;
+                }
+
+                await next(env);
+
+            };
+        }
+    }
+}
---
+++ NancyxUnitAsyncDuplicateRequests/Tests/StartupTests.cs
@@ -0,0 +1,66 @@
+﻿namespace Tests
+{
+    using System;
+    using Xunit;
+    using System.Threading.Tasks;
+    using Microsoft.Owin.Testing;
+    using NancyxUnitAsyncDuplicateRequests;
+    using System.Net.Http;
+
+	public class StartupTests
+	{
+		private const string BaseURL = "http://localhost";
+
+		[Fact]
+		public async Task Test1()
+		{
+			//Given
+			var client = CreateHttpClient();
+
+			//When
+			var response = await client.GetAsync(BaseURL);
+
+			//Then
+			Assert.Equal(200, (int)response.StatusCode);
+		}
+
+        [Fact]
+        public async Task Test2()
+        {
+            //Given
+            var client = CreateHttpClient();
+
+            //When
+            var response = await client.GetAsync(BaseURL);
+
+            //Then
+            Assert.Equal(200, (int)response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Test3()
+        {
+            //Given
+            var client = CreateHttpClient();
+
+            //When
+            var response = await client.GetAsync(BaseURL);
+
+            //Then
+            Assert.Equal(200, (int)response.StatusCode);
+        }
+
+		private HttpClient CreateHttpClient()
+		{
+			var client = TestServer.Create(builder =>
+				new Startup()
+				.Configuration(builder))
+				.HttpClient;
+
+			client.DefaultRequestHeaders.Add("Accept", "application/json");
+		
+			return client;
+		}
+	}
+}
+
